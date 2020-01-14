edc8598caed1fdc221d0536ca37c41168ec76b8a
p-trupthi <p.trupthi@adihptham.com>
2020-01-14 19:18:01 +0530

    Task1

--- src/Main.js
+++ src/Main.js
@@ -8,32 +8,33 @@ export default class Main extends Component {
   render() {
     return (
       <View style={{flex:1, backgroundColor: 'black', alignItems:'center'}} >
-         <Image style={{width:100, height:100}} source={require('./Picture3.png')} ></Image>
-        <Text style={{color:'white'}}>Welcome to Adihptham</Text>
-       <View style={{flexDirection:'row'}}>
-       <Icon name="user" size={30} color='white'/>
+         <Image style={{width:100, height:100, marginTop:20}} source={require('./Picture3.png')} ></Image>
+        <Text style={{color:'white', marginTop:50}}>Welcome to Adihptham</Text>
+       <View style={{flexDirection:'row',borderColor:'white', borderWidth:1, marginTop:40, width:'80%',color:'white'}}>
+       <Icon name="user" size={30} color='white' style={{margin:10}}/>
          <TextInput
-      style={{ height: '40%',width:'80%', borderColor: 'white', borderWidth: 1 }}
+      style={{ width:'80%', borderColor: 'white'}}
       onChangeText={text => onChangeText(text)}
     />
     </View>
-    <View style={{flexDirection:'row'}}>
-    <Icon name="lock" size={30} color='white' />
+    <View style={{flexDirection:'row',borderColor:'white', borderWidth:1, marginTop:30, width:'80%',color:'white'}}>
+    <Icon name="lock" size={30} color='white' style={{margin:10}} />
       <TextInput
-      style={{ height: '40%', width:'80%', borderColor: 'white', borderWidth: 1 }}
+      style={{ width:'80%', borderColor: 'white' }}
       onChangeText={text => onChangeText(text)}
-    />
+      secureTextEntry={true}/>
     </View>
 
-
+        <View style={{margin:'5%', width:'40%', marginBottom:30, marginTop:50}}>
       
          <Button
           title="LOGIN"
           color="skyblue"
           onPress={() => Alert.alert('Button with adjusted color pressed')}
        /> 
+       </View>
       <TouchableOpacity>
-       <Text style={{ color:'white', fontSize: 15 }}>
+       <Text style={{ color:'white',margin:2, fontSize: 15 }}>
        Forgot password?
        </Text>
        </TouchableOpacity>
