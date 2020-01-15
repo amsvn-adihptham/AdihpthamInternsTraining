748ba190c68d2c9523d67f079db4db04f1d448cb
poojapolampalli <pooja.polampalli@adihptham.com>
2020-01-15 13:13:55 +0530

    Task1

--- src/TrainingPhase/Task1.js
+++ src/TrainingPhase/Task1.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Button, TextInput } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Button, TextInput, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Task1 extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
-
+    this.state = {password:''
+    };
     
   }
 
   render() {
     return (
-      <View style={{flex:1, backgroundColor:'black', alignItems:'center'}} >
-        <Image style={{width:100, height:100, margin:'8%'}}
+      <View style={{flex:1, backgroundColor:'black', alignItems:'center', borderRadius:15, borderWidth:2}} >
+
+        <Image style={{width:125, height:125,margin:20, marginBottom:'2%'}}
         source={require('./Picture3.png')}/>
-        <Text style={{color:'white', margin:'8%'}}>WELCOME TO ADIHPTHAM</Text>
-        <View style={{flexDirection:'row', borderColor:'white', borderWidth:1,marginTop:5, width:'80%', color:'white'}}>
+
+        <Text style={{color:'white', margin:'8%', fontWeight:'bold', fontSize:20, textDecorationLine:'underline'}}>Welcome To ADIHPTHAM</Text>
+
+        <View style={{flexDirection:'row', borderColor:'white', borderWidth:1,marginTop:5, marginBottom:15, width:'80%', color:'white'}}>
         <Icon name="user" size={25} color='white' style={{margin:10}}/> 
-        <TextInput style={{color:'white', width:'50%'}}/>
+        <TextInput 
+        placeholder='Number'
+        placeholderTextColor='white'
+        keyboardType={'numeric'}
+        style={{color:'white', width:'50%'}}/>
         </View>
-        <View style={{flexDirection:'row', borderColor: 'white',marginTop:25, marginBottom:25, borderWidth: 1, width:'80%', color:'white'}}>
+
+        <View style={{flexDirection:'row', borderColor:'white', borderWidth:1,marginTop:5, marginBottom:35, width:'80%', color:'white'}}>
         <Icon name="lock" size={25} color='white' style={{margin:10}}/> 
-        <TextInput style={{color:'white', width:'50%'}}
-          secureTextEntry={true} />
+        <TextInput style={{color:'white', width:'80%'}}
+          placeholder='Password'
+          placeholderTextColor='white'
+          secureTextEntry={true}
+                    value={this.state.password}
+                    onChangeText={ (password) => this.setState({ password }) }
+                />
+        <Icon name="eye" size={25} color='grey' style={{margin:10, marginLeft:'-4%'}}/>
          </View>
-         <View style={{margin:'5%', width:'50%', marginBottom:10}}>
-        <Button title="Login" color='steelblue' />
+
+         <View style={{margin:'5%', width:'80%', marginBottom:8}}>
+        <Button title="Login" color='steelblue' 
+        onPress={()=>{alert('You have successfully logged in!');}}/>
         </View>
+
+        <View style={{flexDirection:'row'}}>
         <TouchableOpacity onPress={this.onPress}>
-          <Text style={{color:'white', margin:2}}> Forgot Password?
-            </Text> 
-            </TouchableOpacity> 
+          <Text style={{color:'white',marginLeft:'33%'}}> Forgot Password?
+            </Text > 
+          </TouchableOpacity> 
+
+          <TouchableOpacity onPress={this.onPress}>
+          <Text style={{color:'white'}}> Sign Up
+            </Text>
+          </TouchableOpacity>
+          </View>
+
       </View>
     );
   }
